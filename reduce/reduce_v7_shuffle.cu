11d86b3092d5cd64e7794d87f6aaaa8ece206fbc
Huaijin Zheng <czheng12399@outlook.com>
2025-04-02 23:06:42 +0000

    temp shuffle commit

--- reduce/reduce_v7_shuffle.cu
+++ reduce/reduce_v7_shuffle.cu
@@ -8,46 +8,39 @@
 #define THREAD_PER_BLOCK 256
 #define WARP_SIZE 32
 
-template <unsigned int blockSize>
-__device__ __forceinline__ float warpReduceSum(float sum) {
-    if (blockSize >= 32)sum += __shfl_down_sync(0xffffffff, sum, 16); // 0-16, 1-17, 2-18, etc.
-    if (blockSize >= 16)sum += __shfl_down_sync(0xffffffff, sum, 8);// 0-8, 1-9, 2-10, etc.
-    if (blockSize >= 8)sum += __shfl_down_sync(0xffffffff, sum, 4);// 0-4, 1-5, 2-6, etc.
-    if (blockSize >= 4)sum += __shfl_down_sync(0xffffffff, sum, 2);// 0-2, 1-3, 4-6, 5-7, etc.
-    if (blockSize >= 2)sum += __shfl_down_sync(0xffffffff, sum, 1);// 0-1, 2-3, 4-5, etc.
+// 对warp内32个线程做规约，规约为一个值
+// 规约log32次, 返回结果为warp内的
+template<typename T>
+__device__ T warp_reduce_sum(T sum){
+    for(int stride=WARP_SIZE/2; stride>=1; stride>>=1){
+        sum += __shfl_down_sync(0xffffffff, sum, stride);
+    }
     return sum;
 }
-
-template <unsigned int blockSize, int NUM_PER_THREAD>
-__global__ void reduce7(float *d_in,float *d_out, unsigned int n){
-    float sum = 0;
-
-    // each thread loads one element from global to shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x * (blockSize * NUM_PER_THREAD) + threadIdx.x;
-
-    #pragma unroll
-    for(int iter=0; iter<NUM_PER_THREAD; iter++){
-        sum += d_in[i+iter*blockSize];
-    }
-    
-    // Shared mem for partial sums (one per warp in the block)
-    static __shared__ float warpLevelSums[WARP_SIZE]; 
-    const int laneId = threadIdx.x % WARP_SIZE;
-    const int warpId = threadIdx.x / WARP_SIZE;
-
-    sum = warpReduceSum<blockSize>(sum);
-
-    if(laneId == 0 )warpLevelSums[warpId] = sum;
+template<typename T, int warp_num>
+__global__ void reduce7(T*input, T*output){
+    int tid = threadIdx.x;
+    int src_i = blockDim.x * blockIdx.x + threadIdx.x;
+    __shared__ T shmem[THREAD_PER_BLOCK];
+    shmem[tid] = input[src_i];
     __syncthreads();
-    // read from shared memory only if that warp existed
-    sum = (threadIdx.x < blockDim.x / WARP_SIZE) ? warpLevelSums[laneId] : 0;
-    // Final reduce using first warp
-    if (warpId == 0) sum = warpReduceSum<blockSize/WARP_SIZE>(sum); 
-    // write result for this block to global mem
-    if (tid == 0) d_out[blockIdx.x] = sum;
+    int warp_id = tid / WARP_SIZE;
+    int lane_id = tid % WARP_SIZE;
+    T warp_sum = shmem[tid];
+    warp_sum = warp_reduce_sum<T>(warp_sum);
+    __shared__ T shmem2[warp_num];
+    if(lane_id==0){
+        shmem2[warp_id] = warp_num;
+    }
+    T sum = shmem2[warp_id];
+    // 只有第一个warp进行reduce
+    if(warp_id==0){
+        sum = warp_reduce_sum<T>(sum);
+    }
+    if(tid==0){
+        output[blockIdx.x] = sum;
+    }
 }
-
 
 bool check(float *out,float *res,int n){
     for(int i=0;i<n;i++){
@@ -90,10 +83,7 @@ int main(){
     dim3 Grid( block_num, 1);
     dim3 Block( THREAD_PER_BLOCK, 1);
 
-    int iter = 2000;
-    for(int i=0; i<iter; i++){
-        reduce7<THREAD_PER_BLOCK, NUM_PER_THREAD><<<Grid,Block>>>(d_a, d_out, N);
-    }
+    reduce7<float, THREAD_PER_BLOCK/WARP_SIZE><<<Grid,Block>>>(d_a, d_out);
 
     cudaMemcpy(out,d_out,block_num*sizeof(float),cudaMemcpyDeviceToHost);
 
